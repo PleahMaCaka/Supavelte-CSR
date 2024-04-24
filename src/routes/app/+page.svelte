203b9d6568bb4759de66a5ffe3f8f5d36a241c81
PleahMaCaka <pleahmacaka@gmail.com>
2024-04-25 06:31:25 +0900

    feat: migrate to tauri

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,7 +1,3 @@
-<script>
-    import { Capacitor } from "@capacitor/core"
-</script>
-
-<h1 class="text-center">
-    You are in {Capacitor.getPlatform()}!
+<h1>
+    hi
 </h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { sveltekit } from "@sveltejs/kit/vite"
+import { internalIpV4 } from "internal-ip"
 import { defineConfig } from "vitest/config"
 
+const isMobile =
+    process.env.TAURI_PLATFORM === "android" ||
+    process.env.TAURI_PLATFORM === "ios"
+
 export default defineConfig({
+    clearScreen: false,
     plugins: [sveltekit()],
     test: {
         include: ["tests/**/*.{test,spec}.{js,ts}"]
+    },
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        hmr: isMobile ?? {
+            protocol: "ws",
+            host: await internalIpV4(),
+            port: 5174
+        },
+        strictPort: true
+    },
+    build: {
+        // ES2021
+        target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+        // Prevent minify for debug build
+        minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+        // Produce source maps for debug build
+        sourcemap: !!process.env.TAURI_DEBUG,
     }
 })
