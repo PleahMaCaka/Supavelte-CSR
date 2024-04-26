20a57fd9e8a829440a23e07d6601cce039593d8b
PleahMaCaka <pleahmacaka@gmail.com>
2024-04-26 16:22:28 +0900

    chore(vite): remove old configurations

--- vite.config.ts
+++ vite.config.ts
@@ -8,16 +8,6 @@ export default defineConfig({
         include: ["tests/**/*.{test,spec}.{js,ts}"]
     },
     server: {
-        host: "0.0.0.0",
-        port: 5173,
         strictPort: true
-    },
-    build: {
-        // ES2021
-        target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-        // Prevent minify for debug build
-        minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-        // Produce source maps for debug build
-        sourcemap: !!process.env.TAURI_DEBUG,
     }
 })
