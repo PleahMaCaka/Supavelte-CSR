5638a38909d1b978312fd16b593b2b29036aaf76
PleahMaCaka <pleahmacaka@gmail.com>
2024-08-23 17:25:58 +0900

    chore(deps): add Playwright and rm missed

--- src/stories/auth.stories.ts
+++ src/stories/auth.stories.ts
@@ -4,34 +4,34 @@ import type { Meta, StoryObj } from "@storybook/svelte"
 import AuthField from "@lib/components/auth/AuthField.svelte"
 
 const meta = {
-    title: "Auth/Field",
-    component: AuthField,
-    tags: ["autodocs"]
+  title: "Auth/Field",
+  component: AuthField,
+  tags: ["autodocs"]
 } satisfies Meta<AuthField>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const TextType: Story = {
-    args: {
-        type: "text",
-        label: "Text Type",
-        value: ""
-    }
+  args: {
+    type: "text",
+    label: "Text Type",
+    value: ""
+  }
 }
 
 export const EmailType: Story = {
-    args: {
-        type: "email",
-        label: "Email Type",
-        value: ""
-    }
+  args: {
+    type: "email",
+    label: "Email Type",
+    value: ""
+  }
 }
 
 export const PasswordType: Story = {
-    args: {
-        type: "password",
-        label: "Password Type",
-        value: ""
-    }
+  args: {
+    type: "password",
+    label: "Password Type",
+    value: ""
+  }
 }
