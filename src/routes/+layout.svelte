cbedf09db0a62eaa2f9eed95314efc12173c0d6c
PleahMaCaka <pleahmacaka@gmail.com>
2024-09-08 10:13:01 +0900

    refactor: tauri config and auth handling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
   import "../app.css"
+  import { supa } from "@lib/Supabase"
+  import type { AuthSession } from "@supabase/supabase-js"
+  import { session } from "@stores/session"
+  import { get } from "svelte/store"
+  import { goto } from "$app/navigation"
+
+  const handle = (newSession: AuthSession | null) => {
+    session.set(newSession)
+    if (
+      location.pathname.match(/^\/app[/\w-]*$/) &&
+      (!newSession || get(session) === null)
+    ) {
+      void goto("/signin")
+    }
+  }
+
+  supa.auth.getSession().then(({ data }) => {
+    handle(data.session)
+  })
+
+  supa.auth.onAuthStateChange((_event, _session) => {
+    handle(_session)
+  })
 </script>
 
-<slot />
+<main>
+  <slot />
+</main>
