4614d7cc9eb4a8433d8eb752ba7bec9286fc806b
PleahMaCaka <pleahmacaka@gmail.com>
2024-09-09 07:05:00 +0900

    chore(deps): rm tabler, replace lucide and resolve slow icon deps optimization

--- src/lib/components/auth/AuthField.svelte
+++ src/lib/components/auth/AuthField.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { HTMLInputTypeAttribute } from "svelte/elements"
-  import { IconKey, IconMail } from "@tabler/icons-svelte"
+  import { Key, Mail } from "lucide-svelte"
 
   export let type: HTMLInputTypeAttribute = "text"
   export let label: string = type
@@ -16,9 +16,9 @@
 </span>
 <label class="input input-bordered flex items-center gap-2">
   {#if type === "email"}
-    <IconMail class="size-4 opacity-70" />
+    <Mail class="size-4 opacity-70" />
   {:else if type === "password"}
-    <IconKey class="size-4 opacity-70" />
+    <Key class="size-4 opacity-70" />
   {/if}
   <input bind:value class="grow" name={type} required use:inputType />
 </label>
